bbe78ec904522de37a68f5210b8def582bbae507
Yavuz Avci <yavuzavci01@gmail.com>
2023-01-20 18:02:10 +0200

    updated

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// const btn = document.querySelector("#btnSearch");
-// let text = document.querySelector("#txtSearch");
-// btn.addEventListener("click", () => {
-//     text.value;
-//     getCountry(text);
-// })
-document.querySelector("#btnSearch").addEventListener("click", () => {
-    let text = document.querySelector("#txtSearch").value;
+const btnSearch = document.querySelector("#btnSearch");
+const textInput = document.querySelector("#txtSearch");
+btnSearch.addEventListener("click", () => {
+    let text = textInput.value;
     document.querySelector("#details").style.opacity = 0;
     document.querySelector("#loading").style.display = "block"
     getCountry(text);
 });
+textInput.addEventListener("keydown", (e) => {
+    let text = textInput.value;
+    if (e.keyCode == 13) {
+        getCountry(text);
+    }
+})
 document.querySelector("#btnLocation").addEventListener("click", () => {
     if (navigator.geolocation) {
         document.querySelector("#loading").style.display = "block";
@@ -33,46 +35,6 @@ async function onSuccess(position) {
     document.querySelector("#btnSearch").click();
 
 }
-// function getCountry(country) {
-//     const request = new XMLHttpRequest();
-//     request.open("GET", "https://restcountries.com/v3.1/name/" + country);
-//     request.send();
-//     request.addEventListener("load", function () {
-//         const data = JSON.parse(this.responseText);
-//         renderCountry(data[0]);
-//         // Load Neighbors
-//         const countries = data[0].borders.toString();
-//         const reqst = new XMLHttpRequest();
-//         reqst.open("get", "https://restcountries.com/v3.1/alpha?codes=" + countries);
-//         reqst.send();
-//         reqst.addEventListener("load", function () {
-//             const data = JSON.parse(this.responseText);
-//             renderNeighbors(data);
-//         })
-//     });
-// }
-// function getCountry(country) {
-//     fetch("https://restcountries.com/v3.1/name/" + country)
-//         .then((response) => {
-//             if (!response.ok) {
-//                 throw new Error("No Counrty that name");
-//             }
-//             return response.json();
-//         }).then(data => {
-//             renderCountry(data[0]);
-//             const countries = data[0].borders;
-//             if (!countries) {
-//                 throw Error("There are no neighbors")
-//             }
-//             return fetch("https://restcountries.com/v3.1/alpha?codes=" + countries.toString())
-//         }).then(response => {
-//             return response.json()
-//         }).then(data => {
-//             renderNeighbors(data)
-//         }).catch(err => {
-//             renderError(err)
-//         })
-// }
 async function getCountry(country) {
     try {
         const response = await fetch("https://restcountries.com/v3.1/name/" + country);
@@ -94,6 +56,7 @@ async function getCountry(country) {
         renderError(err)
     }
 }
+
 function renderCountry(data) {
     document.querySelector("#loading").style.display = ("none");
     document.querySelector("#country-details").innerHTML = "";
@@ -112,7 +75,7 @@ function renderCountry(data) {
                 </div>
                 <div class="row">
                     <div class="col-4">Language:</div>
-                    <div class="col-8">${Object.values(data.languages)}</div>
+                    <div class="col-8" id="language">${Object.values(data.languages)}</div>
                 </div>
                 <div class="row">
                     <div class="col-4">Capital:</div>
@@ -126,15 +89,15 @@ function renderCountry(data) {
     `;
     document.querySelector("#details").style.opacity = 1;
     document.querySelector("#country-details").innerHTML = html;
-
 }
+
 function renderNeighbors(data) {
     let html = "";
     for (let country of data) {
         html += `
                 <div class="col-2 mt-2">
                     <div class="card">
-                        <img src="${country.flags.png}" class="card-img-top">
+                        <img data-name="${country.name.common}" src="${country.flags.png}" class="card-img-top">
                         <div class="card-body">
                             <h6 class="card-title">${country.name.common}</h6>
                         </div>
@@ -143,6 +106,7 @@ function renderNeighbors(data) {
             `;
     }
     document.querySelector("#neighbors").innerHTML = html;
+    addEventToFlags();
 }
 function renderError(err) {
     document.querySelector("#loading").style.display = ("none");
@@ -157,3 +121,40 @@ function renderError(err) {
     }, 3000);
     document.querySelector("#errors").innerHTML = html;
 }
+function addEventToFlags() {
+    const countryFlags = document.querySelectorAll("img");
+    countryFlags.forEach(flag => {
+        flag.addEventListener("click", () => {
+            const flagName = flag.dataset.name
+            getCountry(flagName);
+        })
+    })
+}
+function addEventForCountryData(data) {
+    const language = document.querySelector("#language");
+    language.addEventListener("click", () => {
+        getSameLanguageCountryFromFetch(Object.values(data.languages[0]));
+    })
+}
+
+async function getSameLanguageCountryFromFetch(language) {
+    try {
+        const response = await fetch("https://restcountries.com/v3.1/lang/" + language);
+        if (!response.ok) {
+            throw new Error("No country same language")
+        }
+        const data = await response.json();
+        console.log(data);
+    }
+    catch (err) {
+        renderError(err)
+    }
+}
+
+// function addEventToLanguage() {
+//     const countryLanguage = document.querySelector("#language");
+//     countryLanguage.addEventListener("click", getSameLanguageCountry())
+//     console.log(countryLanguage);
+// }
+// addEventToLanguage()
+// }
